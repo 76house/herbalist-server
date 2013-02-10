b97102a9f72b577f4b0e40edc41145289bf40bec
76house <martin.marek@carabana.cz>
2013-02-10 21:39:28 +0100

    CZ translation fixed

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,16 @@ env.hosts = ['bozka@s7.wservices.ch']
 def deploy():
    local('git push')
    run('cd ~/herbalist; git pull')
-   run(' ~/init/herbalist restart')
+   run('export DJANGO_SETTINGS_MODULE=herbalist.settings_deployment')
+   run('./manage.py migrate herbapp')
+   run('./manage.py collectstatic')
+   run('~/init/herbalist restart')
 
 def restart():
-   run(' ~/init/herbalist restart')
+   run('~/init/herbalist restart')
 
 def stop():
-   run(' ~/init/herbalist stop')
+   run('~/init/herbalist stop')
 
 def start():
-   run(' ~/init/herbalist start')
+   run('~/init/herbalist start')
